90731cc825b62535fe093460c2124ccececfddd6
j-thach000 <jamesthach64@gmail.com>
2022-10-15 21:50:45 -0400

    Add completed problem 9

--- practice-3/3_9.cpp
+++ practice-3/3_9.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
 
 /*
-Problem 3.9:
+Problem 3.9: What is the output of the program below?
+
+Same as previous
+x = 3
+x = 5
 */
 int main()
 {
+    int x = 0, count = 0;
 
+    while (count != 2)
+    {
+        if ((x*x - 8*x + 15) == 0)
+        {
+            std::cout << "x = " << x << '\n';
+            count++;
+        }
+        x++;
+    }
 }
